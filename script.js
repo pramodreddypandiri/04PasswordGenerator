9b0ff2a66c7550b9560d4279643170074f4910ca
pramodreddypandiri <pramodreddypandiri010@gmail.com>
2022-09-22 06:59:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const generateEl = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+}
+
+clipboardEl.addEventListener('click', () => {
+    
+})
+
+generateEl.addEventListener('click', () => {
+    // console.log(lengthEl.value);
+    // console.log(uppercaseEl.checked);
+    // console.log(lowercaseEl.checked);
+    // console.log(numbersEl.checked);
+    // console.log(symbolsEl.checked);
+    
+    
+})
+
+function generatePassword(lower, upper, number, symbol, length) {
+    
+}
+
+function getRandomLower() {
+}
+
+function getRandomUpper() {
+}
+
+function getRandomNumber() {
+}
+
+function getRandomSymbol() {
+}
